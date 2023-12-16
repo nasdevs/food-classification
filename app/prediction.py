e526ebb5a4fa29da17441cde49a68ffe6adbd835
nas <me@example.com>
2023-12-16 10:16:10 +0800

    add config file

--- app/prediction.py
+++ app/prediction.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
+from app.config import Config
 from flask import jsonify, request
 from werkzeug.utils import secure_filename
 from PIL import Image
@@ -8,33 +9,29 @@ from app import app
 from app.model_loader import load_custom_model
 from app.nutrition_loader import load_nutrition_data
 
-os.makedirs('static/uploads/', exist_ok=True)
+os.makedirs(Config.upload_folder, exist_ok=True)
 
-model_path = 'model/food-classification-model.h5'
-nutrition_path = 'data/nutrition.csv'
-
-model = load_custom_model(model_path)
-labels, nutrition = load_nutrition_data(nutrition_path)
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-UPLOAD_FOLDER = 'static/uploads/'
+model = load_custom_model(Config.model_path)
+labels, nutrition = load_nutrition_data(Config.nutrition_path)
 
 def allowed_file(filename):
-    return '.' in filename and filename.split('.', 1)[1] in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.split('.', 1)[1] in Config.allowed_extension
 
 @app.route('/prediction', methods=['POST'])
 def prediction():
+    img_size = (224, 224)
+
     if request.method == 'POST':
         image = request.files['image']
         if image and allowed_file(image.filename):
             # Save input image
             filename = secure_filename(image.filename)
-            image_path = os.path.join(UPLOAD_FOLDER, filename)
+            image_path = os.path.join(Config.upload_folder, filename)
             image.save(image_path)
 
             # Pre-processing input  image
             img = Image.open(image_path).convert('RGB')
-            img = img.resize((224, 224))
+            img = img.resize(img_size)
             img_array = np.asarray(img)
             img_array = np.expand_dims(img_array, axis=0)
             normalized_img_array = img_array.astype(np.float32) / 127.5 - 1
@@ -48,7 +45,7 @@ def prediction():
             confidence_score = prediction[0][index]
             nutrition_data = nutrition[index].astype('str')
             
-            if confidence_score < 0.5:
+            if confidence_score < Config.confidence_standard:
                 return jsonify({
                     'status': {
                         'code': 200,
